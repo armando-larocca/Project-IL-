a94f138dd00a5d5b2adc054a770c42ce8cf4a29c
Armando La Rocca <64634645+armando-larocca@users.noreply.github.com>
2020-05-22 10:10:57 +0200

    Creation Cifar100.py

---
+++ cifar100.py
@@ -0,0 +1,119 @@
+from torchvision.datasets import VisionDataset
+from sklearn import preprocessing
+from PIL import Image
+
+import os
+import os.path
+import sys
+
+import numpy as np
+import pickle
+
+import PIL.Image as Image
+from torchvision.transforms import ToTensor, ToPILImage
+import pylab
+
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+class Cifar100(VisionDataset):
+    def __init__(self,root, train=True, transform=None, target_transform=None):
+        super(Cifar100, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        base_folder = 'cifar-100-python'
+        url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
+        filename = "cifar-100-python.tar.gz"
+        tgz_md5 = 'eb9058c3a382ffc7106e4002c42a8d85'
+        train_list = [
+        ['train', '16019d7e3df5f24257cddd939b257f8d'],    
+        ]
+        test_list = [
+        ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
+        ]
+        meta = {
+        'filename': 'meta',
+        'key': 'fine_label_names',
+        'md5': '7973b15100ade9c7d40fb424638fde48',
+        }
+
+        targets=[]
+        data =[]
+        self.train = train 
+
+        download_and_extract_archive(url,self.root, filename=filename, md5=tgz_md5)
+        
+        if self.train:
+            downloaded_list = train_list
+        else:
+            downloaded_list = test_list
+
+        for file_name, checksum in downloaded_list:
+          file_path = os.path.join(self.root,base_folder, file_name)
+
+          with open(file_path, 'rb') as f:
+            entry = pickle.load(f, encoding='latin1')
+            #img = pil_loader(file_path)
+            data.extend(entry['data'])
+
+            data = np.vstack(data).reshape(-1, 3, 32, 32)
+            data = data.transpose((0, 2, 3, 1))
+
+            if 'labels' in entry:
+              targets.extend(entry['labels'])
+            else:
+              targets.extend(entry['fine_labels'])
+            
+        self.data= data
+        self.targets= targets
+
+
+    def __getitem__(self, index):
+
+            image = self.data[index]
+            label = self.targets[index]
+
+            image = Image.fromarray(image)
+
+            if self.transform is not None:
+              image = self.transform(image)
+
+            return image, label
+
+    def __len__(self):
+        
+      length = len(self.data)
+      return length
+     
+    def __get_class_images__(self, label):
+         class_images = []
+         for i in range(0, self.__len__()):
+            if self.targets[i] == label:
+               class_images.append(i)
+         return class_images 
+
+    def __get_number_of_classes__(self):
+       length = len(set(self.targets))
+       return length
+
+    def __train_data_indexes__(self, proportion):
+       images_subset = []
+       for i in range(0,self.__get_number_of_classes__()):
+         class_subset = self.__get_class_images__(i)
+         splitted_class_subset = class_subset[0:int(proportion*len(self.__get_class_images__(i)))]
+         images_subset = images_subset+splitted_class_subset
+       return images_subset
+
+    def __val_data_indexes__(self, proportion):
+       images_subset = []
+       for i in range(0,self.__get_number_of_classes__()):
+         class_subset = self.__get_class_images__(i)
+         splitted_class_subset = class_subset[int(proportion*len(self.__get_class_images__(i))) : len(self.__get_class_images__(i))]
+         images_subset = images_subset+splitted_class_subset
+       return images_subset
+  
